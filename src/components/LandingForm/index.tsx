8903227ad8970d910cf956693e793082f18d2e31
Carlos Alves <carlosalves369m@hotmail.com>
2021-12-14 15:35:13 -0300

    :art: build useState to receive form data

--- src/components/LandingForm/index.tsx
+++ src/components/LandingForm/index.tsx
@@ -1,17 +1,54 @@
+import { useState } from 'react'
 import styles from './styles.module.sass'
 
 export function LandingForm () {
+    const [ contentForm, setContentForm] = useState({
+        name: '',
+        email: '',
+        company: '',
+        website: ''
+    })
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault()
+        alert(`Obrigado ${contentForm.name} por se inscrever na nossa LandPage, verifique se já recebeu na sua na caixa de mensagem e/ou no spam do seu e-mail.`)
+    }
+
     return (
         <aside className={styles.contentWrapper}>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="name">Nome</label>
-                <input type="text" name="name" id="name" />
+                <input 
+                    type="text" 
+                    name="name" 
+                    id="name" 
+                    onChange={(e) =>  setContentForm({...contentForm, name: e.target.value})} 
+                    value={ contentForm.name } 
+                />
                 <label htmlFor="email">E-mail</label>
-                <input type="email" name="email" id="email" />
+                <input 
+                    type="email" 
+                    name="email" 
+                    id="email" 
+                    onChange={(e) =>  setContentForm({...contentForm, email: e.target.value})} 
+                    value={ contentForm.email } 
+                />
                 <label htmlFor="company">Empresa</label>
-                <input type="text" name="company" id="company" />
+                <input 
+                    type="text" 
+                    name="company" 
+                    id="company" 
+                    onChange={(e) =>  setContentForm({...contentForm, company: e.target.value})} 
+                    value={ contentForm.company } 
+                />
                 <label htmlFor="website">Site da empresa</label>
-                <input type="url" name="website" id="website" />
+                <input 
+                    type="url" 
+                    name="website" 
+                    id="website" 
+                    onChange={(e) =>  setContentForm({...contentForm, website: e.target.value})} 
+                    value={ contentForm.website } 
+                />
 
                 <hr />
 
