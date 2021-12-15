3acdc85173a74c4453193068d757d46f451a7379
Carlos Alves <carlosalves369m@hotmail.com>
2021-12-15 15:32:50 -0300

    :alien: Add API connection

--- src/components/LandingForm/index.tsx
+++ src/components/LandingForm/index.tsx
@@ -1,37 +1,50 @@
 import { useState } from 'react'
 import styles from './styles.module.sass'
+import { api } from '../../services/api'
 
 export function LandingForm () {
     const [ contentForm, setContentForm] = useState({
         name: '',
         email: '',
         company: '',
-        website: ''
+        website: '',
+        marketing: false,
+        police: false
     })
 
     const handleSubmit = (e: any) => {
         e.preventDefault()
-        alert(`Obrigado ${contentForm.name} por se inscrever na nossa LandPage, verifique se já recebeu na sua na caixa de mensagem e/ou no spam do seu e-mail.`)
+        
+        api
+            .post('/subscription', contentForm)
+            .then(res => {
+                setContentForm(res.data.id)
+                alert(`Obrigado ${contentForm.name} por se inscrever na nossa LandPage, verifique se já recebeu na sua na caixa de mensagem e/ou no spam do seu e-mail.`)
+            })
+            .catch((err) => alert('Ops, temos um problema com nossos servidores, por favor tente novamente mais tarde.'))
+        
     }
 
     return (
         <aside className={styles.contentWrapper}>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Nome</label>
+                <label htmlFor="name">Nome *</label>
                 <input 
                     type="text" 
                     name="name" 
                     id="name" 
                     onChange={(e) =>  setContentForm({...contentForm, name: e.target.value})} 
                     value={ contentForm.name } 
+                    required
                 />
-                <label htmlFor="email">E-mail</label>
+                <label htmlFor="email">E-mail *</label>
                 <input 
                     type="email" 
                     name="email" 
                     id="email" 
                     onChange={(e) =>  setContentForm({...contentForm, email: e.target.value})} 
                     value={ contentForm.email } 
+                    required
                 />
                 <label htmlFor="company">Empresa</label>
                 <input 
@@ -53,12 +66,25 @@ export function LandingForm () {
                 <hr />
 
                 <div>
-                    <input type="checkbox" name="marketing" id="marketing" />
+                    <input 
+                        type="checkbox" 
+                        name="marketing" 
+                        id="marketing" 
+                        onChange={() => contentForm.marketing == true ? setContentForm({...contentForm, marketing: false}) : setContentForm({...contentForm, marketing: true})} 
+                        checked={ contentForm.marketing }
+                    />
                     <label htmlFor="marketing">Desejo receber e-mail com atualização de material.</label>
                 </div>
                 <div>
-                    <input type="checkbox" name="police" id="police" />
-                    <label htmlFor="police">Li e aceito as <a href="#">políticas de privacidade</a>.</label>
+                    <input 
+                        type="checkbox" 
+                        name="police" 
+                        id="police" 
+                        onChange={() => contentForm.police == true ? setContentForm({...contentForm, police: false}) : setContentForm({...contentForm, police: true})} 
+                        checked={ contentForm.police }
+                        required
+                    />
+                    <label htmlFor="police">Li e aceito as <a href="#">políticas de privacidade</a>. *</label>
                 </div>
                 
                 <div className={styles.button}>
