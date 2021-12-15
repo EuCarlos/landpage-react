bc59f210f8790d7db56d14ed5b9063035bf0334d
Carlos Alves <carlosalves369m@hotmail.com>
2021-12-15 11:13:56 -0300

    :lipstick: Add MessageList Styles

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,20 +1,26 @@
+import messages from '../../database/messageList.json'
+import styles from './styles.module.sass'
+
 export function MessageList() {
     
     return (
-        <section>
-            { messages.map(message => {
-                return(
-                    <li key={message.id} className={styles.message}>
-                        <p className={styles.messageContent}>{message.text}</p>
-                        <div className={styles.messageUser}>
-                            <div className={styles.userImage}>
-                                <img src={message.user.avatar_url} alt={message.user.name} />
+        <div className={styles.MessageListWrapper}>
+
+            <ul className={styles.messageList}>
+                { messages.map(message => {
+                    return(
+                        <li key={message.id} className={styles.message}>
+                            <p className={styles.messageContent}>{message.text}</p>
+                            <div className={styles.messageUser}>
+                                <div className={styles.userImage}>
+                                    <img src={message.user.avatar_url} alt={message.user.name} />
+                                </div>
+                                <span>{message.user.name}</span>
                             </div>
-                            <span>{message.user.name}</span>
-                        </div>
-                    </li>
-                )
-            })}
-        </section>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
     )
 }
