a80b9150975586a72c68d4334d3d16f96a4493f4
Carlos Alves <carlosalves369m@hotmail.com>
2021-12-15 13:18:55 -0300

    :heavy_plus_sign: Config axios to MessageList component

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,11 +1,30 @@
-import messages from '../../database/messageList.json'
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
 import styles from './styles.module.sass'
+
+type Message = {
+    id: string, 
+    text: string, 
+    user: {
+        name: string, 
+        avatar_url: string
+    }
+}
 
 export function MessageList() {
     
+    const [ messages, setMessages ] = useState<Message[]>([])
+
+    useEffect(() => {
+        api
+            .get<Message[]>('/messages')
+            .then((response) => setMessages(response.data))
+            .catch((err) => alert("Ops, ocorreu um erro: " + err))
+    }, [])
+
     return (
         <div className={styles.MessageListWrapper}>
-
+            <h3>Veja alguns comentários sobre o E-book</h3> 
             <ul className={styles.messageList}>
                 { messages.map(message => {
                     return(
